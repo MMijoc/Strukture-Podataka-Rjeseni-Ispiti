9700fda9189db4d50e749a6289e33e9252c2b5df
MMijoc <markomijoc18@gmail.com>
2021-02-08 14:31:51 +0100

    Initial commit

---
+++ Zadatak_03/Zadatak_03/Source.c
@@ -0,0 +1,33 @@
+﻿/*
+Zadatak_03
+29. kolovoza 2019.
+
+Napisati program koji generira 20 slučajnih brojeva u opsegu od 20 - 50.
+Pri tome parne brojeve spremiti na početak vezane liste od najvećeg prema najmanjem,
+a neparne spremati na kraj vezane najmanjeg prema najvećem.
+U vezanoj listi ne smije biti ponavljanja elemenata i ne smiju se koristiti nizovi.
+Ispisati listu.
+
+Složiti elemente vezane liste od najvećeg prema najmanjem, ali se pri tome ne smije koristiti funkcija za sortiranje,
+već se treba iskoristiti raspored elemenata u vezanoj listi i samo prebacivati neparne elemente na odgovarajuće mjesto
+(od na većeg prema najmanjem).
+Ispisati listu.Ž*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define TRUE 1
+#define FALSE 0
+#define SUCCESS 0
+#define FAILURE (-1)
+
+int main()
+{
+
+	puts("Hello World");
+	return SUCCESS;
+
+}
