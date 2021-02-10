311456ccbaf3d845e1ae96e168e001b51bb6e856
MMijoc <markomijoc18@gmail.com>
2021-02-10 09:50:07 +0100

    Added sorted insert function that meets required specification; added PrintList, FreeList; added error related functions

--- Zadatak_03/Zadatak_03/Source.c
+++ Zadatak_03/Zadatak_03/Source.c
@@ -11,7 +11,8 @@ Ispisati listu.
 Složiti elemente vezane liste od najvećeg prema najmanjem, ali se pri tome ne smije koristiti funkcija za sortiranje,
 već se treba iskoristiti raspored elemenata u vezanoj listi i samo prebacivati neparne elemente na odgovarajuće mjesto
 (od na većeg prema najmanjem).
-Ispisati listu.Ž*/
+Ispisati listu.
+*/
 
 #define _CRT_SECURE_NO_WARNINGS
 
@@ -19,15 +20,134 @@ Ispisati listu.Ž*/
 #include <stdio.h>
 #include <string.h>
 
+#include <errno.h>
+
 #define TRUE 1
 #define FALSE 0
 #define SUCCESS 0
 #define FAILURE (-1)
 
+typedef struct _node {
+	int value;
+
+	struct _node *next;
+} node;
+
+int GetRandomNumber(int min, int max);
+node *CreateNewNode(int value);
+int EvenOddInsert(node *listHead, node *toInsert);
+int FreeList(node *listHead);
+int ExecutionFailure(char *message);
+void *ExecutionFailureNull(char *message);
+int PrintList(node *head);
+
+
 int main()
 {
+	int number = 0;
 
-	puts("Hello World");
+	node List = {0, NULL};
+	int i = 0;
+	for (i = 0; i < 20; i++) {
+		number = GetRandomNumber(100, 200);
+		printf("%d ", number);
+		EvenOddInsert(&List, CreateNewNode(number));
+	}
+
+	puts("");
+	PrintList(&List);
+	puts("");
+
+	FreeList(List.next);
 	return SUCCESS;
 
+}
+
+int GetRandomNumber(int min, int max)
+{
+	return min + rand() % (max - min);
+}
+
+node *CreateNewNode(int value)
+{
+	node *newNode = NULL;
+
+	newNode = (node *)malloc(sizeof(node));
+	if (!newNode) return (node *)ExecutionFailureNull("Error");
+
+	newNode->value = value;
+	newNode->next = NULL;
+
+	return newNode;	
+}
+
+int EvenOddInsert(node *listHead, node *toInsert)
+{
+	node *tmp = listHead;
+	
+	if (!listHead || !toInsert) return ExecutionFailure("Invalid functions paramaters");
+
+	tmp = listHead;
+	if (toInsert->value % 2 == 0) {
+		while (tmp->next && tmp->next->value % 2 == 0 && tmp->next->value < toInsert->value)
+			tmp = tmp->next;
+	
+	} else {
+		while (tmp->next && tmp->next->value % 2 == 0)
+			tmp = tmp->next;
+		while (tmp->next && tmp->next->value > toInsert->value)
+			tmp = tmp->next;
+	}
+	
+	if (tmp->next && tmp->next->value == toInsert->value) {
+		free(toInsert);
+		return SUCCESS;
+	}
+
+	toInsert->next = tmp->next;
+	tmp->next = toInsert;
+
+	return SUCCESS;
+}
+
+int PrintList(node *head)
+{
+	node *tmp = head->next;
+
+	while (tmp) {
+		printf("%d ", tmp->value);
+		tmp = tmp->next;
+	}
+
+	return SUCCESS;
+}
+
+int ExecutionFailure(char *message)
+{
+	if (errno != 0)
+		perror(message);
+	else
+		fprintf(stderr,"%s", message);
+
+	return FAILURE;
+}
+
+void *ExecutionFailureNull(char *message)
+{
+	if (errno != 0)
+		perror(message);
+	else
+		fprintf(stderr,"%s", message);
+
+	return NULL;
+}
+
+int FreeList(node *firstToFree)
+{
+	if (firstToFree == NULL) return SUCCESS;
+
+	FreeList(firstToFree->next);
+	free(firstToFree);
+
+	return SUCCESS;
 }
