e6362c8b6a5f8a6b3da4a407042cc721e8b4923c
MMijoc <markomijoc18@gmail.com>
2021-02-11 20:10:41 +0100

    Implemented priority queue

--- Zadatak_07/Zadatak_07/Source.c
+++ Zadatak_07/Zadatak_07/Source.c
@@ -17,20 +17,56 @@ Ispisati red.
 #include <stdlib.h>
 #include <string.h>
 
+//for debugging memory leaks
 #define _CRTDBG_MAP_ALLOC
 #include <crtdbg.h>
 
 #define SUCCESS 0
 #define FAILURE (-1)
+#define N 30
+
+typedef struct _priorityQueue {
+	int value;
+	int priority;
+
+	struct _priorityQueue *next;
+} PriorityQueue;
 
 int ExecutionFailure(char *message);
 void *ExecutionFailureNull(char *message);
-
+int GetRandomNumber(int min, int max);
+PriorityQueue *CreateNewNode(int value, int priority);
+int Push(PriorityQueue *queueHead, PriorityQueue *toInsert);
+int Pop(PriorityQueue *queueHead, int *toPop);
+int PrintList(PriorityQueue *queueHead);
 
 int main()
 {
-	puts("Hello World");
+	int value;
+	int priority;
+	int i, tmp;
+	PriorityQueue Queue = {0, 0, NULL};
 
+	//for debugging memory leaks
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	printf("Values and priority generated in order:");
+	for (i = 0; i < N; i++) {
+		value = GetRandomNumber(100, 200);
+		priority = GetRandomNumber(1, 5);
+		printf("\n%d %d", value, priority);
+		Push(&Queue, CreateNewNode(value, priority));
+	}
+
+	printf("\n\nPriority Queue:");
+	PrintList(&Queue);
+
+	printf("\n\nPoping all values form Priority Queue:");
+	while (Pop(&Queue, &tmp) != FAILURE) {
+		printf("%d -> ", tmp);
+	}
+
+	puts("");
 	return SUCCESS;
 }
 
@@ -53,3 +89,70 @@ void *ExecutionFailureNull(char *message)
 
 	return NULL;
 }
+
+int GetRandomNumber(int min, int max)
+{
+	return min + rand() % (max - min + 1);
+}
+
+PriorityQueue *CreateNewNode(int value, int priority)
+{
+	PriorityQueue *newNode = NULL;
+	
+	newNode = (PriorityQueue *)malloc(sizeof(PriorityQueue));
+	if (!newNode) return (PriorityQueue *)ExecutionFailureNull("Error");
+
+	newNode->value = value;
+	newNode->priority = priority;
+	newNode->next = NULL;
+
+	return newNode;
+}
+
+int Push(PriorityQueue *queueHead, PriorityQueue *toInsert)
+{
+	PriorityQueue *tmp = NULL;
+	
+	if (!queueHead || !toInsert) return ExecutionFailure("Invalid function arguments");
+
+	tmp = queueHead;
+	while (tmp->next && tmp->next->priority < toInsert->priority)
+		tmp = tmp->next;
+
+	while (tmp->next && tmp->next->priority == toInsert->priority && tmp->next->value < toInsert->value)
+		tmp = tmp->next;
+
+	toInsert->next = tmp->next;
+	tmp->next = toInsert;
+
+	return SUCCESS;
+}
+
+int Pop(PriorityQueue *queueHead, int *toPop)
+{
+	PriorityQueue *tmp = NULL;
+
+	tmp = queueHead->next;
+	if (tmp == NULL) return FAILURE;
+
+	queueHead->next = tmp->next;
+	*toPop = tmp->value;
+	free(tmp);
+
+	return SUCCESS;
+}
+
+int PrintList(PriorityQueue *queueHead)
+{
+	PriorityQueue *tmp = queueHead->next;
+	while (tmp) {
+		printf("\n%d %d", tmp->value, tmp->priority);
+		tmp = tmp->next;
+	}
+
+	return SUCCESS;
+}
+
+
+
+
