a5092719366dd63c45e2cce733f45a8119b42583
MMijoc <markomijoc18@gmail.com>
2021-02-11 10:00:30 +0100

    Implemented required specification

--- Zadatak_05/Zadatak_05/Source.c
+++ Zadatak_05/Zadatak_05/Source.c
@@ -23,14 +23,179 @@ Ispisati novu vezanu listu kao i broj ponavljanja svakog čvora.
 #define FALSE 0
 #define SUCCESS 0
 #define FAILURE (-1)
-#define BUFFER_LENGTH 1024
+#define N 30
+
+typedef struct _node {
+	int value;
+	int nDuplicates;
+
+	struct _node *next;
+} node;
+
+int GetRandomNumber(int min, int max);
+node *CreateNewNode(int value);
+node *FindTail(node *listHead);
+int InsertAtTail(node *listHead, node *toInsert);
+int PrintList(node *listHead);
+int DeleteDuplicates(node *listHead);
+int SotredInsert(node *listHead, node *toInsert);
+int FreeList(node *node);
+int ExecutionFailure(char *message);
+void *ExecutionFailureNull(char *message);
 
 int main()
 {
+	node List = {0, 0, NULL};
+	int numbers[N];
+	int i = 0;
+
 	//for debuging memory leaks
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	printf("Hello World\n");
+	printf("Randomly generated numbers are:\n\t");
+	for (i = 0; i < N; i++) {
+		numbers[i] = GetRandomNumber(120, 150);
+		printf("%d ", numbers[i]);
+	}
+
+	for (i = 0; i < N; i++)
+		InsertAtTail(&List, CreateNewNode(numbers[i]));
+
+	printf("\nRandom numbers in the list:\n\t");
+	PrintList(&List);
+
+	DeleteDuplicates(&List);
+	printf("\nList with no duplicate values in it (number in the brackets is the number of duplicate values in the original list):\n\t");
+	PrintList(&List);
+
+	FreeList(List.next);
+	puts("");
+	return SUCCESS;
+}
+
+int GetRandomNumber(int min, int max)
+{
+	return min + rand() % (max - min);
+}
+
+node *CreateNewNode(int value)
+{
+	node *newNode = NULL;
+
+	newNode = (node *)malloc(sizeof(node));
+	if (!newNode) return (node *)ExecutionFailureNull("Error");
+
+	newNode->value = value;
+	newNode->nDuplicates = 1;
+	newNode->next = NULL;
+
+	return newNode;
+}
+
+node *FindTail(node *listHead)
+{
+	node *tmp = listHead;
+
+	while (tmp->next)
+		tmp = tmp->next;
+
+	return tmp;
+}
+
+int InsertAtTail(node *listHead, node *toInsert)
+{
+	node *tail = NULL;
+
+	if (!listHead || !toInsert) return ExecutionFailure("Invalid function paramaters");
+
+	tail = FindTail(listHead);
+	tail->next = toInsert;
 
 	return SUCCESS;
+}
+
+int PrintList(node *listHead)
+{
+	node *tmp = listHead->next;
+
+	while (tmp) {
+		printf("%d", tmp->value);
+		if (tmp->nDuplicates != 1)
+			printf("(%d)",tmp->nDuplicates);
+		printf(" ");
+		tmp = tmp->next;
+	}
+
+	return SUCCESS;
+}
+
+int DeleteDuplicates(node *listHead)
+{
+	node *tmp = NULL;
+	node *nextOne = NULL;
+	node *tmpList = NULL;
+
+	tmp =  listHead->next;
+	listHead->next = NULL; //break list
+
+	while (tmp) {
+		nextOne = tmp->next;
+		SotredInsert(listHead, tmp);
+		tmp = nextOne;
+	}
+
+	return SUCCESS;
+}
+
+int SotredInsert(node *listHead, node *toInsert)
+{
+	node *tmp = NULL;
+	node *toFree = NULL;
+
+	if (!listHead || !toInsert) return ExecutionFailure("Invalid function paramters");
+
+	tmp = listHead;
+	while (tmp->next && tmp->next->value < toInsert->value)
+		tmp = tmp->next;
+
+	if (tmp->next && tmp->next->value == toInsert->value) {
+		tmp->next->nDuplicates++;
+		free(toInsert);
+		return SUCCESS;
+	}
+
+	toInsert->next = tmp->next;
+	tmp->next = toInsert;
+
+	return SUCCESS;
+}
+
+int FreeList(node *node)
+{
+	if (node == NULL) return SUCCESS;
+
+	FreeList(node->next);
+	free(node);
+
+	return SUCCESS;
+}
+
+int ExecutionFailure(char *message)
+{
+	if (errno != 0)
+		perror(message);
+	else
+		fprintf(stderr, "\n%s", message);
+
+	return FAILURE;
+}
+
+void *ExecutionFailureNull(char *message)
+{
+	if (errno != 0)
+		perror(message);
+	else
+		fprintf(stderr, "\n%s", message);
+
+	return NULL;
 }
