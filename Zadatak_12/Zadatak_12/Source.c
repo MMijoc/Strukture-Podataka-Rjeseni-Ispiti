6d5451d33da81429cf89cbf945bf5f70830687bb
MMijoc <markomijoc18@gmail.com>
2021-02-13 19:50:56 +0100

    Inital commit

---
+++ Zadatak_12/Zadatak_12/Source.c
@@ -0,0 +1,45 @@
+/*
+
+
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+
+#define SUCCESS 0
+#define FAILURE (-1)
+#define TRUE 1
+#define FALSE 0
+#define BUFFER_LENGTH 1024
+
+#define RETURN_FAILURE(message) do {PrintError(message); return FAILURE;} while(0);
+#define RETURN_NULL(message) do {PrintError(message); return NULL;} while(0);
+#define ERR_0 "Invalid function arguments"
+
+void PrintError(char *message);
+
+int main()
+{
+
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	puts("hello world");
+
+	return SUCCESS;
+}
+
+void PrintError(char *message)
+{
+	if (errno != 0)
+		perror(message);
+	else
+		fprintf(stderr, "\n%s", message);
+
+	return;
+}
