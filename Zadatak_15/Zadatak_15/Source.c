83e90f28bb5c8afec22b9b09ba5bab2cf37b2889
MMijoc <markomijoc18@gmail.com>
2021-02-15 17:44:01 +0100

    Implemented taks

--- Zadatak_15/Zadatak_15/Source.c
+++ Zadatak_15/Zadatak_15/Source.c
@@ -23,21 +23,58 @@ Unos traje sve dok obje liste ne sadrže 10 clemenata. Treba ispisivati koji se 
 #define TRUE 1
 #define FALSE 0
 #define BUFFER_LENGTH 1024
+#define MAX 10
+
+typedef struct _node {
+	int value;
+
+	struct _node *next;
+} node;
 
 #define RETURN_FAILURE(message) do {PrintError(message); return FAILURE;} while(0);
 #define RETURN_NULL(message) do {PrintError(message); return NULL;} while(0);
+#define ERR_0 "Invalid function arguments"
 
 
 void PrintError(char *message);
-
+int GetRandomNumber(int min, int max);
+node *CreateNewNode(int value);
+int SotredInsert(node *listHead, node *toInsert);
+int PrintList(node *listHead);
+int FreeList(node *node);
 
 int main()
 {
+	node even = {0, NULL};
+	node odd = {0, NULL};
+	int x = 0;
+	int evenCount = 0;
+	int oddCount = 0;
+
 
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	puts("Hello world");
+	while ((evenCount < MAX) || (oddCount < MAX)) {
+		x = GetRandomNumber(50, 80);
+		if (x % 2 == 0) {
+			if (evenCount < MAX)
+				if (SotredInsert(&even, CreateNewNode(x)) == SUCCESS)
+					evenCount++;
+		} else {
+			if (oddCount < MAX)
+				if (SotredInsert(&odd, CreateNewNode(x)) == SUCCESS)
+					oddCount++; 
+		}
+	}
 
+	printf("Even list:\t ");
+	PrintList(&even);
+	printf("\nOdd list:\t ");
+	PrintList(&odd);
+	puts("");
+
+	FreeList(even.next);
+	FreeList(odd.next);
 	return SUCCESS;
 
 }
@@ -50,4 +87,65 @@ void PrintError(char *message)
 		fprintf(stderr, "\n%s", message);
 
 	return;
+}
+
+int GetRandomNumber(int min, int max)
+{
+	return min + rand() % (max - min);
+}
+
+node *CreateNewNode(int value)
+{
+	node *newNode = NULL;
+
+	newNode = (node *)malloc(sizeof(node));
+	if (!newNode) RETURN_NULL("Error");
+
+	newNode->value = value;
+	newNode->next = NULL;
+
+	return newNode;	
+}
+
+int SotredInsert(node *listHead, node *toInsert)
+{
+	node *tmp = NULL;
+
+	if (!listHead || !toInsert) RETURN_FAILURE(ERR_0);
+
+	tmp = listHead;
+	while (tmp->next && tmp->next->value > toInsert->value)
+		tmp = tmp->next;
+
+	if (tmp->next && tmp->next->value == toInsert->value) {
+		free(toInsert);
+		return FAILURE;
+	}
+
+	toInsert->next = tmp->next;
+	tmp->next = toInsert;
+
+	return SUCCESS;
+}
+
+int PrintList(node *listHead)
+{
+	node *tmp = listHead->next;
+
+	while (tmp) {
+		printf("%d ", tmp->value);
+		tmp = tmp->next;
+	}
+
+	return SUCCESS;
+}
+
+int FreeList(node *node)
+{
+	if (node == NULL) return SUCCESS;
+
+	FreeList(node->next);
+	free(node);
+
+	return SUCCESS;
 }
