f2258a19ec03df4a9d63d0eaca078fd8d3e8a9fa
MMijoc <markomijoc18@gmail.com>
2021-02-15 15:10:51 +0100

    Initial commit

---
+++ Zadatak_16/Zadatak_16/Source.c
@@ -0,0 +1,62 @@
+﻿/*
+Zadatak_16
+dekanski rok 23.9.2014.
+
+
+Napisati program koji iz dvije datoteke (jedna sadrži ocjenu 1. kolokvija,a druga ocjenu 2. kolokvija) kreira vezanu listu studenata koji su položili ispit
+i ta se nova vezana lista slaže po ocjenama. Način pretvorbe bodova u ocjenu je dan u tablici.
+
+Bodovi			Ocjena
+50 - 59.99%		2
+
+60- 73.99%		3
+
+74 - 86.99%		4
+
+87 - 100		5
+
+
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+
+#define SUCCESS 0
+#define FAILURE (-1)
+#define TRUE 1
+#define FALSE 0
+#define BUFFER_LENGTH 1024
+
+#define RETURN_FAILURE(message) do {PrintError(message); return FAILURE;} while(0);
+#define RETURN_NULL(message) do {PrintError(message); return NULL;} while(0);
+
+
+void PrintError(char *message);
+
+
+int main()
+{
+
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	puts("Hello world");
+
+	return SUCCESS;
+
+}
+
+void PrintError(char *message)
+{
+	if (errno != 0)
+		perror(message);
+	else
+		fprintf(stderr, "\n%s", message);
+
+	return;
+}
